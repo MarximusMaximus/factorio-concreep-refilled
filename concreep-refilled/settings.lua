f297908ee8ff82c8b9668722a6d72ed21938d6c1
NocturnalScream <a-weber-weber@web.de>
2022-07-26 10:59:07 +0200

    Feature update - surface filter
    - Added an option to specify the surfaces concreep should be running on (by name)
    - added various options to be changed at runtime, i.e. update rate, speed etc.

--- concreep-refilled/settings.lua
+++ concreep-refilled/settings.lua
@@ -58,6 +58,51 @@ data:extend({
 		order = "040"
     },
 	{
+        type = "int-setting",
+        name = "minimum robot",
+        setting_type = "runtime-global",
+        default_value = 30,
+        minimum_value = 0,
+        maximum_value = 100000,
+        order = "041"
+	},
+	{
+        type = "int-setting",
+        name = "minimum item",
+        setting_type = "runtime-global",
+        default_value = 200,
+        minimum_value = 1,
+        maximum_value = 100000000,
+        order = "042"
+	},
+	{
+        type = "int-setting",
+        name = "RUN_EVERY_N_UPDATES",
+        setting_type = "runtime-global",
+        default_value = 60,
+        minimum_value = 1,
+        maximum_value = 1000,
+        order = "043"
+	},
+	{
+        type = "int-setting",
+        name = "CREEPERS_PER_UPDATE",
+        setting_type = "runtime-global",
+        default_value = 5,
+        minimum_value = 1,
+        maximum_value = 1000,
+        order = "044"
+	},
+	{
+        type = "int-setting",
+        name = "ROBOPORTS_PER_REINIT_PASS",
+        setting_type = "runtime-global",
+        default_value = 1,
+        minimum_value = 1,
+        maximum_value = 1000,
+        order = "045"
+	},
+	{
 		type = "bool-setting",
 		name = "debug",
 		setting_type = "runtime-global",
@@ -77,5 +122,14 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = false,
 		order = "992"
+    },
+	{
+		type = "string-setting",
+		name = "surface concreep is running on",
+		setting_type = "runtime-global",
+		default_value = "nauvis",
+		auto_trim = true,
+		allow_blank = false,
+		order = "993"
     },
 })
