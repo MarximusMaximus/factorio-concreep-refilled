7a2e6a94ceaea0e69c5cb21171dd7a20fc493a36
Marximus Maximus <marximus@marximus.com>
2022-08-25 16:28:07 -0700

    cleanup build scripts, use own mods folder to run

--- make_release.py
+++ make_release.py
@@ -4,16 +4,30 @@ import json
 import os
 import os.path
 import subprocess
+import sys
+import typing
 
-os.chdir(os.path.dirname(__file__))
+def make_release() -> typing.Tuple[int, str]:
+    my_abs_path = os.path.abspath(__file__)
+    my_folder_abs_path = os.path.dirname(my_abs_path)
 
-name = "concreep-refilled"
+    os.chdir(my_folder_abs_path)
 
-f = open(f"{name}/info.json")
-text = f.read()
-data = json.loads(text)
-version = data["version"]
+    folder_name = os.path.basename(my_folder_abs_path)
+    mod_name = folder_name.removeprefix("factorio-")
 
-os.makedirs("build", exist_ok=True)
+    f = open(f"{mod_name}/info.json")
+    text = f.read()
+    data = json.loads(text)
+    version = data["version"]
 
-subprocess.run(["zip", "-vr", f"build/{name}_{version}.zip", f"{name}"])
+    os.makedirs("build", exist_ok=True)
+
+    outfile = f"build/{mod_name}_{version}.zip"
+
+    res = subprocess.run(["zip", "-vr", outfile, f"{mod_name}"])
+
+    return res.returncode, outfile
+
+if __name__ == "__main__":
+    sys.exit(make_release())
